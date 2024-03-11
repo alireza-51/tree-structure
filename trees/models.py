30ee730463ee5c277173af342fb4096ea3316a8f
alireza-51 <alirezazarei51@gmail.com>
2024-03-11 22:37:33 +0330

    create Node model in tree app

--- trees/models.py
+++ trees/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Node(models.Model):
+    attribute_name = models.CharField(max_length=64)
+    parent = models.ForeignKey(
+        'self', 
+        null=True, 
+        blank=True, 
+        related_name='children', 
+        on_delete=models.CASCADE
+    )
